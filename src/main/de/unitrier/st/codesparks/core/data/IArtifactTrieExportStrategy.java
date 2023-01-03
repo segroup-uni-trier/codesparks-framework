686b2db7c8b2cc62120462badf89319298a8a536
Oliver Moseler <moseler@uni-trier.de>
2023-01-03 18:54:21 +0100

    moved artifact trie classes to jpt, removed unnecessary dependencies, added python-psi dependency including a new jetbrains resolver, introduced ivy settings, new jar tasks

--- src/main/de/unitrier/st/codesparks/core/data/IArtifactTrieExportStrategy.java
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2021. Oliver Moseler
- */
-package de.unitrier.st.codesparks.core.data;
-
-public interface IArtifactTrieExportStrategy
-{
-    void export(ArtifactTrie artifactTrie);
-}
