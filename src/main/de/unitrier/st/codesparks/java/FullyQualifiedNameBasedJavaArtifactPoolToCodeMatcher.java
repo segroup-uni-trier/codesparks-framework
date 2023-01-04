9c6373cb7bafa752beda058713f6be4657b9de57
Oliver Moseler <moseler@uni-trier.de>
2023-01-04 17:17:18 +0100

    update copyright, apache 2.0 license

--- src/main/de/unitrier/st/codesparks/java/FullyQualifiedNameBasedJavaArtifactPoolToCodeMatcher.java
+++ src/main/de/unitrier/st/codesparks/java/FullyQualifiedNameBasedJavaArtifactPoolToCodeMatcher.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2022. Oliver Moseler
+ *  Copyright 2023 Oliver Moseler
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package de.unitrier.st.codesparks.java;
 
@@ -24,8 +36,6 @@ import java.util.Map;
  * It is assumed that each artifact is identified by its fully qualified name and stored in
  * the artifact pool correspondingly. That is, getIdentifier() should yield an artifact's fully
  * qualified name and consequently, an artifact can be retrieved (looked up) in the artifact pool by its identifier.
- *
- * @author Oliver Moseler
  */
 public final class FullyQualifiedNameBasedJavaArtifactPoolToCodeMatcher extends AArtifactPoolToCodeMatcher
 {
--- src/main/de/unitrier/st/codesparks/java/JavaPsiClassNameHelper.java
+++ src/main/de/unitrier/st/codesparks/java/JavaPsiClassNameHelper.java
@@ -1,5 +1,17 @@
 /*
- * Copyright (c) 2021. Oliver Moseler
+ *  Copyright 2023 Oliver Moseler
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 package de.unitrier.st.codesparks.java;
 
@@ -9,9 +21,6 @@ import de.unitrier.st.codesparks.core.logging.CodeSparksLogger;
 import java.util.HashMap;
 import java.util.Map;
 
-/**
- * @author Oliver Moseler on 22.09.2014. Updated on 28.12.2019.
- */
 final class JavaPsiClassNameHelper
 {
     private final Map<PsiClass, Integer> anonymousLevelIndices;
